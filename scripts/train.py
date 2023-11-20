0a89a8799621ebf39b2a84b145fef60e61d3be26
Christian Kasim Loan <christian.kasim.loan@gmail.com>
2023-11-20 20:07:17 +0100

    update train script to support simple CLI

--- scripts/train.py
+++ scripts/train.py
@@ -1,6 +1,19 @@
 import torch
-
+import argparse
 from src.baseline import train, BaselineModel
 
+# Initialize parser
+parser = argparse.ArgumentParser(description="Train a baseline model")
+
+# Adding optional arguments
+parser.add_argument("-d", "--dataset", type=str, default="QM9", help="Dataset to use (QM9, MD17, ISO17)")
+parser.add_argument("-e", "--epochs", type=int, default=50, help="Number of epochs")
+parser.add_argument("-lr", "--learning_rate", type=float, default=0.001, help="Learning rate")
+
+# Read arguments from command line
+args = parser.parse_args()
+
 print("CUDA AVAILABLE:", torch.cuda.is_available())
-train(BaselineModel(), 'QM9', 50)
+
+# Call the train function with CLI arguments
+train(BaselineModel(), dataset=args.dataset, epochs=args.epochs, lr=args.learning_rate)
