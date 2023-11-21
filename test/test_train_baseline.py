ff01a08c77d7a08c4c5ac694bfcf0877a8966c2e
Christian Kasim Loan <christian.kasim.loan@gmail.com>
2023-11-21 04:33:04 +0100

    refactors and fix inconsistent .to(device) usage

--- test/test_train_baseline.py
+++ test/test_train_baseline.py
@@ -13,13 +13,15 @@ def test_id(test: Trainable) -> str:
 # @pytest.mark.parametrize("trainable", qm9_trainable, ids=test_id)
 @pytest.mark.parametrize("trainable", all_trainable_one_molecule, ids=test_id)
 def test_train(trainable: Trainable):
-    print("Training...")
-    n_train = 100
-    model, test_losses = train(model='baseline', dataset=trainable.dataset, task=trainable.task,
-                               molecule=trainable.molecule,
-                               epochs=2, n_train=n_train)
-    if trainable.dataset == Dataset.md17:
-        #     TODO validation for other models
-        print("Validation...")
-        val_loss = validate(model, trainable.dataset, trainable.task, n_train=n_train, molecule=trainable.molecule)
-        print(val_loss)
+    train_and_validate(trainable,'baseline')
+    #
+    # print("Training...")
+    # n_train = 100
+    # model, test_losses = train(model='baseline', dataset=trainable.dataset, task=trainable.task,
+    #                            molecule=trainable.molecule,
+    #                            epochs=2, n_train=n_train)
+    # if trainable.dataset == Dataset.md17:
+    #     #     TODO validation for other models
+    #     print("Validation...")
+    #     val_loss = validate(model, trainable.dataset, trainable.task, n_train=n_train, molecule=trainable.molecule)
+    #     print(val_loss)
