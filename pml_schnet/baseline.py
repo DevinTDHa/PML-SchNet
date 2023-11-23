85521725106a487142ebeac81fe328ecb8bbe206
Devin Ha <t.ha@tu-berlin.de>
2023-11-23 21:19:53 +0100

    Train Baseline Force

--- pml_schnet/baseline.py
+++ pml_schnet/baseline.py
@@ -7,7 +7,7 @@ from torch import nn
 from tqdm import tqdm
 
 from pml_schnet.data_loader import load_data
-from pml_schnet.loss import energy_force_loss
+from pml_schnet.loss import energy_force_loss, derive_force
 from pml_schnet.settings import Dataset, Model, Task, Trainable, valid_molecules
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -139,6 +139,38 @@ def train_baseline_energy(model, n_train, n_test, lr, epochs, dataset):
             X_batch["R"] = X_batch["R"].to(device).float()
             y_batch = y_batch.to(device)
             loss = criterion(model(X_batch), y_batch)
+            # Backward pass and optimization
+            optimizer.zero_grad()  # Clear gradients
+            loss.backward()  # Compute gradients
+            optimizer.step()  # Update weights
+        print(f"Epoch {epoch + 1}, Train Loss: {loss:.4f}")
+        losses.append({"epoch": epoch, "loss": loss.item()})
+    plot_loss(losses)
+    return losses[-1]["loss"]
+
+
+def train_baseline_force(model, n_train, n_test, lr, epochs, dataset):
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    criterion = nn.MSELoss()
+    losses = []
+    model.train()
+    for epoch in tqdm(range(epochs)):
+        train_gen, test_gen = load_data(dataset, n_train, n_test)
+        loss = None
+        for X_batch, y_batch in train_gen:
+            # Forward pass
+            X_batch["N"] = X_batch["N"].to(device).long()
+            X_batch["Z"] = X_batch["Z"].to(device).long()
+            X_batch["R"] = X_batch["R"].to(device).float()
+
+            target_F = X_batch["F"].to(device)
+            target_F.requires_grad_()
+
+            E_pred = model(X_batch)
+
+            F_pred = derive_force(E_pred, X_batch["R"])
+
+            loss = criterion(F_pred, target_F)
             # Backward pass and optimization
             optimizer.zero_grad()  # Clear gradients
             loss.backward()  # Compute gradients
