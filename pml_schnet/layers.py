fix bug with force loss calculation

pml_schnet/layers.py
@@ -4,7 +4,7 @@ from torch import nn, Tensor
 from schnetpack.nn.scatter import scatter_add
 
 
-from settings import device
+from .settings import device
 
 
 class RadialBasisFunctions(nn.Module):

pml_schnet/model.py
@@ -1,10 +1,27 @@
 from typing import Dict
 
 import torch
+
+
 from torch import nn
 
-from layers import SchNetInteraction
+from .layers import SchNetInteraction
 from pml_schnet.activation import ShiftedSoftPlus
+class PairwiseDistances(nn.Module):
+    """
+    Compute pair-wise distances from indices provided by a neighbor list transform.
+    """
+
+    def forward(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        R = inputs['R']
+        offsets = inputs['_offsets']
+        idx_i = inputs['idx_i']
+        idx_j = inputs['idx_j']
+
+        Rij = R[idx_j] - R[idx_i]  #+ offsets # TODO?
+        d_ij = torch.norm(Rij, dim=-1)
+        # inputs[properties.Rij] = Rij
+        return d_ij
 
 
 class BaselineModel(nn.Module):
@@ -74,20 +91,23 @@ class SchnetNet(nn.Module):
             activation(),
             nn.Linear(32, 1),
         )
-
+        self.pairwise = PairwiseDistances()
     def forward(self, inputs: Dict):
-        Z = inputs["Z"]
-        N = inputs["N"]
-        R_distances = inputs["d"]
-        idx_i = inputs["idx_j"]
-        idx_j = inputs["idx_j"]
+        Z = inputs["Z"] # Atomic numbers. Label
+
+        N = inputs["N"] # Number of atoms in each molecule
+        # AKA R_ij/  vectors pointing from center atoms to neighboring atoms
+        # R_distances = inputs["d"]
+
+        R_distances = self.pairwise(inputs)
+
+        idx_i = inputs["idx_i"] # indices of center atoms
+        idx_j = inputs["idx_j"] # indices of neighboring atoms
 
         # 1) Embedding 64 ( see section Molecular representation).
         X = self.embedding(Z)
 
-        # 2) Interaction 64
-        # 3) Interaction 64
-        # 4) Interaction 64
+        # 2),3),4) each Interaction 64 with recurrent layers
         X_interacted = X
         for interaction in self.interactions:
             X_interacted = interaction(X_interacted, R_distances, idx_i, idx_j)

test/test_train_schnet.py
@@ -7,8 +7,8 @@ from torch import nn, autograd
 from torch.nn import ELU
 from tqdm import tqdm
 
-from data_loader import load_data
-from loss import energy_force_loss
+from pml_schnet.data_loader import load_data
+from pml_schnet.loss import energy_force_loss
 from pml_schnet.model import SchnetNet
 from pml_schnet.settings import (
     Trainable,
@@ -116,14 +116,14 @@ def test_data_gen(indexed_data):
     print(indexed_data)
 
 
-def test_inference(indexed_data):
+def test_inference_dummy_data(indexed_data):
     model = SchnetNet(activation=ELU)
 
     res = model(indexed_data)
     print(res)
 
 
-def test_train_schnet():
+def test_train_schnet_energy():
     model = SchnetNet(activation=ELU).to(device)
     lr = 0.1
 
@@ -149,7 +149,7 @@ def test_train_schnet():
     return losses[-1]["loss"]
 
 
-def test_train_schnet_force():
+def test_train_schnet_force_and_energy():
     model = SchnetNet(activation=ELU).to(device)
     lr = 0.1
 
@@ -159,7 +159,7 @@ def test_train_schnet_force():
     epochs = 5
     with autograd.detect_anomaly():
         for epoch in tqdm(range(epochs)):
-            train_gen, test_gen = load_data("MD17", 10000, 10, batch_size=128)
+            train_gen, test_gen = load_data("MD17", 5, 3, batch_size=2)
             loss = None
             for X_batch, y_batch in train_gen:
                 # Forward pass