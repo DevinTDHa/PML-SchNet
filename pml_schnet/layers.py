b3b5903568a77fa38bdf656d7585514716bd6c91
Devin Ha <t.ha@tu-berlin.de>
2024-01-30 09:23:43 +0100

    add RMSNorm

--- pml_schnet/layers.py
+++ pml_schnet/layers.py
@@ -411,3 +411,55 @@ class SchNetInteractionBNDropout(nn.Module):
         X_residual = X + V
 
         return X_residual
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, d, p=-1.0, eps=1e-8, bias=False):
+        """
+        Root Mean Square Layer Normalization.
+
+        From # https://github.com/bzhangGo/rmsnorm/blob/master/rmsnorm_torch.py
+
+        Parameters
+        ----------
+        d : int
+            model size
+        p : float, optional
+            partial RMSNorm, valid value [0, 1], default -1.0 (disabled)
+        eps : float, optional
+            epsilon value, default 1e-8
+        bias : bool, optional
+            Whether use bias term for RMSNorm, disabled by default because RMSNorm doesn't enforce re-centering invariance.
+        """
+        super(RMSNorm, self).__init__()
+
+        self.eps = eps
+        self.d = d
+        self.p = p
+        self.bias = bias
+
+        self.scale = nn.Parameter(torch.ones(d))
+        self.register_parameter("scale", self.scale)
+
+        if self.bias:
+            self.offset = nn.Parameter(torch.zeros(d))
+            self.register_parameter("offset", self.offset)
+
+    def forward(self, x):
+        if self.p < 0.0 or self.p > 1.0:
+            norm_x = x.norm(2, dim=-1, keepdim=True)
+            d_x = self.d
+        else:
+            partial_size = int(self.d * self.p)
+            partial_x, _ = torch.split(x, [partial_size, self.d - partial_size], dim=-1)
+
+            norm_x = partial_x.norm(2, dim=-1, keepdim=True)
+            d_x = partial_size
+
+        rms_x = norm_x * d_x ** (-1.0 / 2)
+        x_normed = x / (rms_x + self.eps)
+
+        if self.bias:
+            return self.scale * x_normed + self.offset
+
+        return self.scale * x_normed
